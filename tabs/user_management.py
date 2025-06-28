a655f5bccefac0263d8fde8ca7e8f770fccb083d
luis7fc <luis7fc@icloud.com>
2025-06-28 13:41:54 -0700

    Update user_management.py

--- tabs/user_management.py
+++ tabs/user_management.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import bcrypt
+import os
 from supabase import create_client, Client
 
 # Initialize Supabase
-SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 st.title("👤 User Management")
